ab6b9b1d8d64b91d935454e94f8cb5e80ad8af35
Bear_fu <303761829@qq.com>
2015-12-05 18:02:54 +0800

    逻辑优化

--- Deduplication.py
+++ Deduplication.py
@@ -3,19 +3,26 @@ __author__ = 'bear_fu'
 
 
 def _Deduplication(NameA, NameB):
-	print("开始去重")
+	start_num = 0
+	max_num = 995998
+	print("开始对文件" + NameA + "去重")
 	obuff = []
 	for ln in open(NameA, encoding="utf-8"):
 		if ln in obuff:
+			start_num = start_num + 1
 			continue
 		obuff.append(ln)
+		# 导入进度条---------------------------------------------------------------------------------------------------
+		import test
+
+		test._Progress_Bar(start_num, max_num)
+	# ————————————————————————————————————————————————————————
 	with open(NameB, 'w', encoding="utf-8") as handle:
 		handle.writelines(obuff)
 		print("去重终了")
+		print("去重结果存放在" + NameB)
 
 
 if __name__ == '__main__':
-	print("去重开始")
-	_Deduplication("JD_commodity_urls", "JD_commodity_url")
-	print("去重终了")
+	_Deduplication("JD_commodity_urls.txt", "JD_commodity_urls.log")
 
